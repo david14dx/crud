f5470c744ff7af5d5554d756841b97f7c02457dd
david14dx <147641797+david14dx@users.noreply.github.com>
2023-10-17 22:21:32 +0800

    tenTwentyOne

--- timekeep.php
+++ timekeep.php
@@ -4,82 +4,64 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <title>Time Keep</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>Document</title>
+    <link rel="stylesheet" href="/style.css" class="rel">
 
-    <style>
-        .gradient-custom {
-            /* fallback for old browsers */
-            background: #f6d365;
-
-            /* Chrome 10-25, Safari 5.1-6 */
-            background: -webkit-linear-gradient(to right bottom, rgba(255, 255, 255, 1), rgba(0, 255, 255, 1));
-
-            /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-            background: linear-gradient(to right bottom, rgba(0, 200, 150, 1), rgba(0, 200, 255, 1))
-        }
-    </style>
 </head>
 
-<body>
-<div class="container-fluid">   </div> 
-
-
-    <section class="vh-100" style="background-color: #f4f5f7;">
-        <div class="container py-5 h-100">
-            <div class="row d-flex justify-content-center align-items-center h-100">
-                <div class="col col-lg-12 mb-4 mb-lg-0">
-                    <div class="card mb-3" style="border-radius: .5rem;">
-                        <div class="row g-0">
-                            <div class="col-md-4 gradient-custom text-center text-white" style="border-top-left-radius: .5rem; border-bottom-left-radius: .5rem;">
-                                <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava1-bg.webp" alt="Avatar" class="img-fluid my-5" style="width: 80px;" />
-                                <h5>Marie Horwitz David </h5>
-                                <p>Web Designer</p>
-
-                                <i class="far fa-edit mb-5"></i>
-                            </div>
-                            <div class="col-md-8">
-                                <div class="card-body p-4">
-                                    <h6>Information</h6>
-                                    <hr class="mt-0 mb-4">
-                                    <div class="row pt-1">
-                                        <div class="col-6 mb-3">
-                                            <h6>Email</h6>
-                                            <p class="text-muted">info@example.com</p>
-                                        </div>
-                                        <div class="col-6 mb-3">
-                                            <h6>Phone</h6>
-                                            <p class="text-muted">123 456 789</p>
-                                        </div>
-                                    </div>
-                                    <h6>Projects</h6>
-                                    <hr class="mt-0 mb-4">
-                                    <div class="row pt-1">
-                                        <div class="col-6 mb-3">
-                                            <h6>Recent</h6>
-                                            <p class="text-muted">Lorem ipsum</p>
-                                        </div>
-                                        <div class="col-6 mb-3">
-                                            <h6>Most Viewed</h6>
-                                            <p class="text-muted">Dolor sit amet</p>
-                                        </div>
-                                    </div>
-                                    <div class="d-flex justify-content-start">
-                                        <a href="#!"><i class="fab fa-facebook-f fa-lg me-3"></i></a>
-                                        <a href="#!"><i class="fab fa-twitter fa-lg me-3"></i></a>
-                                        <a href="#!"><i class="fab fa-instagram fa-lg"></i></a>
-                                    </div>
-                                </div>
-                            </div>
-
-                        </div>
+<body class="bg-info">
+    <div class="container-fluid">
+        <div class="row mt-5">
+            <div class="col"></div>
+            <div class="col-11 text-bg-light ">
+                <p class="h1 h1david text-center">Immaculate Conception I-College of Arts and Technology</p>
+                <p class="h1 text-center">#47 A. Bonifacio St. Poblacion Santa Maria Bulacan</p>
+            </div>
+            <div class="col"></div>
+        </div>
+        <div class="row">
+            <div class="col"></div>
+            <div class="col-sm-4 bg-info-subtle text-danger-emphasis">
+                <p class="h1 h1david"> DATE : <?php echo date("M-d-Y"); ?>
+                </p>
+            </div>
+            <div class="col-sm-7 bg-info-subtle text-danger-emphasis">
+                <p class="h1 h1david"> TIME : <?php date_default_timezone_set('Asia/Manila');
+                                                echo date(" h : i : s a"); ?>
+                </p>
+            </div>
+            <div class="col"></div>
+        </div>
+        <div class="row">
+            <div class="col"></div>
+            <div class="col-sm-4 bg-light">
+                <img src="images/ICILogo.jpg" class="img-fluid" alt="Responsive image" width="100%">
+            </div>
+            <div class="col-sm-7 bg-body-tertiary">
+                <div class="row mt-3">
+                    <div class="col bg-primary-subtle"></div>
+                    <div class="col-sm-11">
+                        <p class="h1 h1david"> Employee ID : </p>
+                        <p class="h1 h1david"> Name : </p>
+                        <p class="h1 h1david"> Position : </p>
+                        <p class="h1 h1david"> Department : </p>
+                        <p class="h1 h1david"> Time Stamp : </p>
                     </div>
+                    <div class="col bg-primary-subtle"></div>
                 </div>
             </div>
+            <div class="col"></div>
         </div>
-    </section>
+        <div class="row">
+            <div class="col"></div>
+            <div class="col-11 bg-info-subtle">
+                <p class="h1"> https://iciap.edu.ph/ | Telephone No. (044) 795-9609 </p>
+            </div>
+            <div class="col"></div>
+        </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 
 </html>
